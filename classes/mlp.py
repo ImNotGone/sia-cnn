cf2ae527b1ccb3cbf1ee3846fb05c52a3fd6308f
Santiago Ballerini <sballerini@itba.edu.ar>
2023-11-30 15:38:15 -0300

    feat:implement fully connected layer

--- classes/mlp.py
+++ classes/mlp.py
@@ -2,11 +2,52 @@ from numpy import ndarray
 import numpy as np
 
 class MLP():
-    def __init__(self):
-        pass
+
+    # TODO: configurar la funcion de activacion
+    def sigmoid(self, input: ndarray):
+        return 1 / (1 + np.exp(-input))
+
+    def sigmoid_prime(self, input: ndarray):
+        return self.sigmoid(input) * (1 - self.sigmoid(input))
+
+    def __init__(self, input_size: int, output_size: int):
+        self.input_size = input_size
+        self.output_size = output_size
+
+        # Para guardar los valores de entrada y salida
+        self.input = np.zeros(input_size)
+        self.excitements = np.zeros(output_size)
+        self.output = np.zeros(output_size)
+
+        self.weights = np.random.randn(input_size, output_size)
 
     def foward_prop(self, input: ndarray):
-        pass
+        self.input = input
+
+        excitements = input @ self.weights
+        activations = self.sigmoid(excitements)
+
+        self.output = activations
+        self.excitements = excitements
+
+        return activations
 
     def back_prop(self, loss_gradient:ndarray, learning_rate: float):
-        pass
+
+        # Loss en funcion de los excitements
+        gradient_excitements = loss_gradient * self.sigmoid_prime(self.excitements)
+
+        # Loss en funcion de los pesos
+        # Reshape para que sea una matriz de dim (input_size, output_size)
+        gradient_weights = self.input.reshape(-1, 1) @ gradient_excitements.reshape(1, -1)
+
+        # Por ahora gradient descent
+        self.weights -= learning_rate * gradient_weights
+
+        # Loss en funcion de la entrada,
+        gradient_input = gradient_excitements @ self.weights.T
+
+        return gradient_input
+
+
+
