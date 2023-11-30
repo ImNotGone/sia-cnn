f49c0115ae63b6dc627bb612d04aeb899874731b
Gonzalo Martone <gmartone@itba.edu.ar>
2023-11-30 10:08:41 -0300

    feat: cr foward_prop

--- classes/cr.py
+++ classes/cr.py
@@ -1,5 +1,6 @@
 import numpy as np
 from enum import Enum
+from numpy import ndarray
 
 class Padding(Enum):
     VALID = 0
@@ -11,6 +12,9 @@ class CR:
     def __init__(self, qty__filters: int, filter_size: int, padding: Padding = Padding.VALID):
         self.qty_filters = qty__filters
         self.filter_size = filter_size
+        
+        # cache for back_prop
+        self.last_input_image = None
 
         # array of qty_filters matrices of filter_size * filter_size
         # Divided by filter_size ^ 2 to reduce variance
@@ -18,15 +22,15 @@ class CR:
 
         self.padding = padding
 
-    def iterate_image_regions(self, input_image):
+    def iterate_image_regions(self, input_image: ndarray):
         # generates matrices of filter_size * filter_size
         # for each section of the image, so that filters are aplied
 
         heigth, width = input_image.shape
         match(self.padding):
             case(Padding.VALID):
-                heigth -= self.filter_size // 2 # floored divition
-                width  -= self.filter_size // 2 # floored divition
+                heigth -= (self.filter_size // 2)*2 # floored divition
+                width  -= (self.filter_size // 2)*2 # floored divition
             case(Padding.SAME):
                 raise "Unimplemented"
             case(Padding.FULL):
@@ -40,5 +44,28 @@ class CR:
                 image_region = input_image[i:(i+self.filter_size), j:(j+self.filter_size)]
                 yield image_region, i, j
 
-    def foward_prop():
+    def foward_prop(self, input_image: ndarray):
+        # aplies the qty_filters filters to the input image
+
+        heigth, width = input_image.shape
+        match(self.padding):
+            case(Padding.VALID):
+                heigth -= (self.filter_size // 2)*2 # floored divition
+                width  -= (self.filter_size // 2)*2 # floored divition
+                print(heigth, width, self.filter_size, (self.filter_size // 2)*2)
+                print(self.filters)
+            case(_):
+                raise 'Unimplemented'
+
+        # cache'd for easier back_prop
+        self.last_input_image = input_image
+
+        output = np.zeros((heigth, width, self.qty_filters))
+
+        for image_region, i, j in self.iterate_image_regions(input_image):
+            output[i, j] = np.sum(image_region * self.filters, axis=(1, 2))
+    
+        return output
+
+    def back_prop(self, loss_gradient, learn_rate):
         pass
