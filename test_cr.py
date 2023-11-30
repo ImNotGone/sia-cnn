15f011187cf44d6be0348eac0f3380553012ada3
Gonzalo Martone <gmartone@itba.edu.ar>
2023-11-30 10:08:57 -0300

    feat: test cr foward_prop

---
+++ test_cr.py
@@ -0,0 +1,27 @@
+from classes.cr import CR
+import numpy as np
+
+cr = CR(1, 3)
+
+filter = [
+    [-1, 0, 1], 
+    [-2, 0, 2], 
+    [-1, 0, 1]
+]
+
+cr.filters[0] = np.array(filter)
+
+# 4 x 4
+image = [
+    [0, 50, 0, 29],
+    [0, 80, 31, 2],
+    [33, 90, 0, 75],
+    [0, 9, 0, 95]
+]
+
+# 1(filter) x 2 x 2
+expected = np.array([[[29, -192], [-35, -22]]])
+actual = cr.foward_prop(np.array(image))
+print(f"image: {image}")
+print(f"expected: {expected}")
+print(f"actual: {actual}")
