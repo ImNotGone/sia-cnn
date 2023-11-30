9d3189d0def35d1e95337970eab30cd50a51ae04
Gonzalo Martone <gmartone@itba.edu.ar>
2023-11-30 09:30:55 -0300

    chore: cr, iterator by image regions

--- classes/cr.py
+++ classes/cr.py
@@ -10,6 +10,7 @@ class Padding(Enum):
 class CR:
     def __init__(self, qty__filters: int, filter_size: int, padding: Padding = Padding.VALID):
         self.qty_filters = qty__filters
+        self.filter_size = filter_size
 
         # array of qty_filters matrices of filter_size * filter_size
         # Divided by filter_size ^ 2 to reduce variance
@@ -17,8 +18,27 @@ class CR:
 
         self.padding = padding
 
-    def iterator_regions():
-        pass
+    def iterate_image_regions(self, input_image):
+        # generates matrices of filter_size * filter_size
+        # for each section of the image, so that filters are aplied
+
+        heigth, width = input_image.shape
+        match(self.padding):
+            case(Padding.VALID):
+                heigth -= self.filter_size // 2 # floored divition
+                width  -= self.filter_size // 2 # floored divition
+            case(Padding.SAME):
+                raise "Unimplemented"
+            case(Padding.FULL):
+                raise "Unimplemented"
+            case(_):
+                raise "Unimplemented"
+            
+
+        for i in range(heigth):
+            for j in range(width):
+                image_region = input_image[i:(i+self.filter_size), j:(j+self.filter_size)]
+                yield image_region, i, j
 
     def foward_prop():
         pass
