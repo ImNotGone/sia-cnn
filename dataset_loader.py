2b607dfecddff6973601b0621fed0fa932712ea0
Santiago Ballerini <sballerini@itba.edu.ar>
2023-11-30 21:49:10 -0300

    feat: make labels one-hot encoded

--- dataset_loader.py
+++ dataset_loader.py
@@ -1,40 +1,45 @@
-import cv2, numpy as np, os
+import cv2
+import numpy as np
+import os
 
-#TODO check if we have to set the cwd
-#parameters
-img_size = 50 #TODO check this
-train_qty= 30 #change this value to alter the amount of images used for training in the set
+# TODO check if we have to set the cwd
+# parameters
+img_size = 50  # TODO check this
+train_ratio = 0.8  # TODO check this
+
 
 def load_dataset():
-    #get data TODO complete folders array with the name of the folder where the images are
-    folders, labels, images = [], [], []
+    folders = ["data/shapes/square", "data/shapes/triangle"]
+    labels = []
+    images = []
+
     for folder in folders:
-        for path in os.listdir(os.getcwd()+'/'+folder):
-            img = cv2.imread(folder+'/'+path,0)
+        for path in os.listdir(os.getcwd() + "/" + folder):
+            img = cv2.imread(folder + "/" + path, 0)
             images.append(cv2.resize(img, (img_size, img_size)))
             labels.append(folders.index(folder))
 
-    #Separate data into training sets and testing sets
-    i=0
-    train_images, train_labels, test_images, test_labels = [],[],[],[]
-    for image, label in zip(images, labels):
-        if i<train_qty:
-            train_images.append(image)
-            train_labels.append(label)
-            i+=1
-        else:
-            test_images.append(image)
-            test_labels.append(label)
+    # Separate data into training sets and testing sets
+    train_qty = int(len(images) * train_ratio)
 
-    #We flatten the data
-    #Training set first
+    # Make labels one-hot encoded,
+    # e.g. 0 -> [1, 0], 1 -> [0, 1]
+    labels = np.eye(len(folders))[labels]
+
+    train_images = images[:train_qty]
+    train_labels = labels[:train_qty]
+
+    test_images = images[train_qty:]
+    test_labels = labels[train_qty:]
+
+    # Training set first
     train_images = np.array(train_images)
-    train_images = train_images.astype('float32')
-    train_images /=255
+    train_images = train_images.astype("float32")
+    train_images /= 255
 
-    #Testing set second
+    # Testing set second
     test_images = np.array(test_images)
-    test_images = test_images.astype('float32')
-    test_images /=255
+    test_images = test_images.astype("float32")
+    test_images /= 255
 
     return train_images, train_labels, test_images, test_labels
