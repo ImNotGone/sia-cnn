d10afa70947a288721c0a3ae324d5396aee292a3
Gonzalo Martone <gmartone@itba.edu.ar>
2023-12-02 19:04:24 -0300

    feat: add test for relu

---
+++ tests/test_relu.py
@@ -0,0 +1,26 @@
+from layers.relu import Relu        
+import numpy as np
+
+r = Relu((1, 2, 2))
+input = np.array([[
+    [1, -1],
+    [-3, 4],
+]])
+actual = r.forward_prop(input)
+expected = np.array([[
+    [1, 0],
+    [0, 4],
+]])
+print(f"expected FP:\n{expected}")
+print(f"actual:\n{actual}")
+loss = np.array([
+    [5, 12],
+    [13, 7],
+])
+actual = r.back_prop(loss)
+expected = np.array([
+    [5, 0],
+    [0, 7],
+])
+print(f"expected BP:\n{expected}")
+print(f"actual:\n{actual}")
